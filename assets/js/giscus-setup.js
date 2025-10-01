fd028c0d7f048a212a57a5a22821126a39d78314
aditvishnu <127286828+aditvishnu@users.noreply.github.com>
2025-10-01 15:55:19 +0000

    Deploying to gh-pages from @ MLLabIISc/mllabiisc.github.io@fc83686dd5d648b564387eccf1b1bf2088f59884 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "MLLabIISc/MLLabIISc",
+    "data-repo": "MLLabIISc/mllabiisc.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
